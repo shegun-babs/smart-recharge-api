fe5c52c0daf41300547dcd14e9447357d31d0a5c
Shegun Babs <shegun.babs@gmail.com>
2021-06-10 23:59:21 +0100

    added doc-blocks for direct-data methods

--- src/Services/DirectData.php
+++ src/Services/DirectData.php
@@ -10,6 +10,10 @@ class DirectData extends AbstractService
     protected string $path = "directdata/?";
 
 
+    /**
+     * @param array $params ['product_code' => '...', 'phone' => '...']
+     * @return array
+     */
     public function buyData(array $params): array
     {
         $params = array_merge(['api_key' => $this->apikey], $params);
@@ -20,6 +24,10 @@ class DirectData extends AbstractService
     }
 
 
+    /**
+     * @param $order_id
+     * @return array
+     */
     public function checkDataOrder($order_id): array
     {
         $params = ['api_key' => $this->getApiKey(), 'task' => 'check_status', 'order_id' => $order_id];
